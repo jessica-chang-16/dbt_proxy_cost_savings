34e52529d640358cfd77e33eecd0a12caf232d03
jessica-chang-16 <jessica.chang@edited.com>
2026-04-10 17:14:03 -0600

    feat: first successful staging model

---
+++ models/staging/stg_cost_data.sql
@@ -0,0 +1,15 @@
+{{config(materialized ='table')}}
+
+WITH raw_client_data as
+(
+SELECT *
+FROM {{source('raw_data','raw_client_data')}}
+),
+renamed as (
+    SELECT 
+    `company name` as retailer,
+    slug as brand
+    FROM raw_client_data
+)
+SELECT *
+FROM renamed
