db13c39ee7aaa984cd505f776178cdf31bbeea45
jessica-chang-16 <jessica.chang@edited.com>
2026-07-22 22:31:54 -0400

    create stg and int models for exchange rates. Also added packages

--- models/staging/stg_proxy_requests.sql
+++ models/staging/stg_proxy_requests.sql
@@ -1,9 +1,19 @@
--- cast date into month
--- select all columns
--- aggregation will happen in intermediate mart with joining exchange rate
-
-WITH requests AS
+WITH base AS
     (SELECT *
     FROM {{source('csv_proxy_requests', 'proxy_requests_amount')}}
     ),
 
+casting AS
+    (SELECT
+    FORMAT_DATE('%B', DATETIME(date)) as month,
+    crawler_slug as brand,
+    SPLIT(crawler_slug,'-') [OFFSET(1)] AS region_abv,
+    proxy_type,
+    http_requests as num_requests,
+    rate_usd_per_1000_requests,
+    cost_usd
+    FROM base)
+
+SELECT 
+    *
+FROM casting
